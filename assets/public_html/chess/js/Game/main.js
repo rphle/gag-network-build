3b4a0d4659516ecbff3b55d8ee254530d38a624b
Dr-Lego <Dr_Lego@ist-einmalig.de>
2024-11-17 10:26:13 +0100

    build 241117_102612

--- assets/public_html/chess/js/Game/main.js
+++
@@ -1,7 +0,0 @@
-const HTML = new HTML_()
-const Game = new Game_(["human","human"],"Standard")
-const Chessboard = new Chessboard_()
-
-
-Game.startGame()
-
--- assets/public_html/geschichte/_app/immutable/entry/start.B95hxodk.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.vg6_01ck.js";export{t as start};
